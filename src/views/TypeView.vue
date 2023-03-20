d9aeca97dd47861284cd605c741a98cd35c1ce68
brokiem <milyhqvideo@gmail.com>
2023-03-20 15:22:06 +0800

    Add github stargazers count

--- src/views/TypeView.vue
+++ src/views/TypeView.vue
@@ -12,6 +12,10 @@
         </svg>
         <a href="/" class="text-4xl font-bold text-center"> TyperX </a>
         <div class="grow"></div>
+        <span class="mr-1">
+          {{ stars_count }}
+        </span>
+        <svg v-if="stars_count !== null" xmlns="http://www.w3.org/2000/svg" height="20" width="20" fill="currentColor" class="inline mr-2"><path d="m5.062 18 1.313-5.542L2 8.729l5.75-.5L10 3l2.25 5.25 5.75.479-4.375 3.729L14.938 18 10 15.062Z"/></svg>
         <a href="https://github.com/brokiem/typerx" target="_blank">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
@@ -101,7 +105,8 @@ export default defineComponent({
       start_time: null as number | null,
       end_time: null as number | null,
       time_elapsed_interval_id: undefined as number | undefined,
-      is_end: false
+      is_end: false,
+      stars_count: null as number | null,
     }
   },
   setup() {
@@ -140,6 +145,13 @@ export default defineComponent({
     }
   },
   mounted() {
+    this.getStarsCount("brokiem", "typerx").then((stars_count) => {
+      // @ts-ignore
+      this.stars_count = stars_count
+    }).catch((error) => {
+      console.log(error)
+    })
+
     this.textStore.$subscribe((mutation, state) => {
       const word_element = document.getElementById("word-" + this.active_word_id)
       const char_element = word_element?.children[this.active_text_id]
@@ -336,6 +348,19 @@ export default defineComponent({
           this.textStore.isTextProcessing = false
           this.textStore.text = "Error loading text"
         })
+    },
+    getStarsCount(owner: string, repo: string) {
+      return new Promise((resolve, reject) => {
+        fetch(`https://api.github.com/repos/${owner}/${repo}`)
+          .then((response) => response.json())
+          .then((data) => {
+            const starCount = data.stargazers_count
+            resolve(starCount)
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
     }
   }
 })
